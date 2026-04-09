c6af884c6d3c5deb640ab813770cae515d16812a
Juan071825 <juanmateoalvarez@gmail.com>
2026-04-09 09:13:51 +0200

    refactor(ArnoldEnumTypes.java): :recycle: Facilitar la lectura del mensaje salido por consola.

--- src/main/java/edu/teamrocket/ArnoldEnumTypes.java
+++ src/main/java/edu/teamrocket/ArnoldEnumTypes.java
@@ -13,7 +13,8 @@ public class ArnoldEnumTypes {
         } 
         catch (NumberFormatException exception){
             System.err.println("Peso incorrecto, no es un número.");
-            System.out.println("Se establece el valor por defecto 1.");
+            System.out.println("Se establece el valor por defecto 1.0.");
+            System.out.println("--------------------------------------");            
             peso = 1.0;
         }
 
